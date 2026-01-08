a70b7529153898942758d40a0c188b42a14840b2
Martin Sapag <mairuba@gmail.com>
2026-01-08 10:40:52 -0300

    Create initial Streamlit app with greeting message
    Primera App publicada

---
+++ streamlit_app.py
@@ -0,0 +1,8 @@
+import streamlit as st
+
+st.title("¡Hola Tincho!")
+st.write("Esta es mi primera app desplegada desde GitHub.")
+st.write("Aquí irá mi primera App publicda.")
+
+# Si quieres subir un archivo csv más tarde:
+# uploaded_file = st.file_uploader("Elige un archivo")
